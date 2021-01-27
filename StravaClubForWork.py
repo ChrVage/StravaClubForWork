6f41933f1add26247be84aa7642dd6fbe2d26eed
Christian Våge <Chr@Vage.com>
2021-01-27 22:02:58 +0100

    Skrev om til funksjoner
    la til config.json

--- StravaClubForWork.py
+++ StravaClubForWork.py
@@ -4,9 +4,7 @@ import pandas as pd
 import json
 from datetime import datetime
 
-# Verifiser at å legge inn et par aktiviteter tilbake i tid for å se om de dukker opp før noe som allerede er i listen
-# Del opp i funksjoner
-# Les inn Config fil med minste mulige data, men data om admin-bruker-token, siste dato 
+# Les inn Config fil med minste mulige data, men client_id, client_secret, refresh_token, siste dato 
 # Legg inn alle datoer etter den som er lagret i config-fil som skjult aktivitet på Atea Strava Admin med dato hver midnatt. yyyy.mm.dd#StravaClubForWork_Date
 # Les ut ny fil fra API, lag ID kolonne og dato kolonne
 #  Lag ID kolonne bestående av navn#sekunder aktivitet#distanse (Det som ikke endrer seg.).
@@ -23,75 +21,95 @@ from datetime import datetime
 #   * Fjern aktiviteter som er mindre enn 900 sekunder
 #   * Gi alle aktiviteter random nummer, laveste vinner.
 
+def authenticate(client_id,client_secret,refresh_token):
+    payload = {
+    'client_id': client_id,
+        'client_secret': client_secret, 
+        'refresh_token': refresh_token,
+        'grant_type': "refresh_token",
+       'f':'json'
+    }
 
-auth_url = "https://www.strava.com/oauth/token"
-activities_url = "https://www.strava.com/api/v3/clubs/10971/activities"
-members_url = ""
+    auth_url = "https://www.strava.com/oauth/token"
 
-payload = {
-    'client_id': "60365",
-    'client_secret': 'd6eaed2e3d1f1d8a154044d5984769536dc06674', 
-    'refresh_token': '18a7f1b532db6ffb0085055a93831b5ad5f4b00a',
-    'grant_type': "refresh_token",
-    'f':'json'
-}
+    return requests.post(auth_url,data=payload, verify=False).json()['access_token']
 
-access_token = requests.post(auth_url,data=payload, verify=False).json()['access_token']
-print('Access token:"%s"\n' % access_token)
+def create_dates_by_clubadmin(date):
+    pass
 
-header = {'Authorization': 'Bearer ' + access_token}
+def get_new_activities(access_token,club_id):
+    # create dictionary of user activities
+    activities = pd.DataFrame(
+        columns = [
+                "Athlete",
+                "Name",
+                "Type",
+                "Duration",
+                "Distance",
+                "Date",
+                "id"
+        ]
+    )
 
-now = datetime.now().strftime("%Y.%m.%d %H%M")
-FileName = 'Activitylist %s.xlsx' % now
+    loop = True
 
-fullname = list()
-activityname = list()
-activitytype = list()
-activityduration = list()
-activitydistance = list()
-fullLine = list()
-allColumns = ['Name', 'Activity', 'Type', 'Duration', 'Distance']
-loop = True
+    readpage = 1
+    pagesize = 50
+    activity_count = 0
+    activities_url = "https://www.strava.com/api/v3/clubs/%s/activities" % club_id
+    activity_row = 0
+    while loop:
+        header = {'Authorization': 'Bearer ' + access_token}
+        param  = {'per_page': pagesize, 'page': readpage}
+        response = requests.get(activities_url,headers=header, params=param)
+        data = response.json()
 
-readpage = 1
-pagesize = 50
-activities = 0
-
-while loop:
-    param = {'per_page': pagesize, 'page': readpage}
-    response = requests.get(activities_url,headers=header, params=param)
-
-    readpage = readpage + 1
-    data = response.json()
-    activities = activities + len(data)
-
-    for line in data :
-        fullname =          fullname +          [line['athlete']['firstname'] +"#"+ line['athlete']['lastname']]
-        activityname =      activityname +      [line['name']]
-        activitytype =      activitytype +      [line['type']]
-        activityduration =  activityduration +  [line['elapsed_time']]
-        activitydistance =  activitydistance +  [line['distance']]
-        
-        # Break in right place
-        taglist = line['name'].split("#")
-        print("taglist[0]: %s" % taglist[0])
-
-        # if taglist[0] in ("UkeStart","") :
-        #     loop = False
-        #     break
+        readpage = readpage + 1
+        activity_count = activity_count + len(data)
     
-    print("data has %d items" % len(data))
-    print("readpage: %i" % readpage)
-    if activities>300: 
-        loop = False
+        for line in data :
+            activities.at[activity_row, 'Athlete']  = line['athlete']['firstname'] +"#"+ line['athlete']['lastname']
+            activities.at[activity_row, 'Name']     = line['name']
+            activities.at[activity_row, 'Type']     = line['type']
+            activities.at[activity_row, 'Duration'] = line['elapsed_time']
+            activities.at[activity_row, 'Distance'] = line['distance']
+            activities.at[activity_row, 'Date']     = datetime.now().strftime("%Y.%m.%d")
+            activities.at[activity_row, 'id']       = "%s#%s#%s" % (activities.at[activity_row, 'Athlete'], activities.at[activity_row, 'Duration'], activities.at[activity_row, 'Distance'])
 
-    if len(data)<pagesize :
-        loop = False
+            activity_row = activity_row + 1
+ 
+        print("Page: %i, len(data): %d " % (readpage-1,len(data)))
+        # https://stackoverflow.com/questions/17071871/how-to-select-rows-from-a-dataframe-based-on-column-values
+        if activity_count>300: 
+            loop = False
 
-print("activities found: %i" % activities)
+        if len(data)<pagesize :
+            loop = False
+    
+    print("activities found: %i" % activity_count)
+    return activities
 
-TheData = list(zip(fullname,activityname,activitytype,activityduration,activitydistance))
+def main():
+    # Read config.json file
+    with open("config.json", "r") as jsonfile:
+        config = json.load(jsonfile)
 
-# Write data to an Excel spreadsheet pr week
-df = pd.DataFrame(data=TheData, columns=allColumns)
-df.to_excel(FileName, index=False)
+    access_token = authenticate(config["client_id"],config["client_secret"],config["refresh_token"])
+    print('Access token:"%s"\n' % access_token)
+    date = config['last_date']
+    print('Date:%s' % date)
+    # date = create_dates_by_clubadmin(date)
+    activities = get_new_activities(access_token,config["club_id"])
+
+    # Write data to an Excel spreadsheet pr week
+    FileName = 'Activitylist %s.xlsx' % datetime.now().strftime("%Y.%m.%d %H%M")
+    
+    df = pd.DataFrame(activities)
+    df.to_excel(FileName, index=False)
+    
+    with open("config.json", "w") as jsonfile:
+        myJSON = json.dump(config, jsonfile, indent=2) # Writing to the file
+        jsonfile.close()
+
+if __name__ == "__main__":
+    main()
